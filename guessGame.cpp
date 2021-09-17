91d5e938190fdad5b254a3d1f3b9e6a253dde6ef
Daria Bernice <daria.bernice.calitis@mths.ca>
2021-09-17 18:23:46 +0000

    updated guessGame.cpp

---
+++ guessGame.cpp
@@ -0,0 +1,31 @@
+// Copyright (c) 2021 D.B. Calitis All rights reserved
+//
+// Created by: Daria Bernice Calitis
+// Created on: Sept 2021
+// This program is a number guessing game
+
+
+#include <iostream>
+
+
+int main() {
+    // This function checks if your guess is correct
+
+
+    const int ANSWER = 5;
+    int guessNumber;
+
+    // input
+    std::cout << "Enter a number as your guess (0-9): ";
+    std::cin >> guessNumber;
+
+    // process & output
+    if (guessNumber == ANSWER) {
+        std::cout << "You guessed correctly!" << std::endl;
+    } else {
+        std::cout << "You guessed incorrectly." << std::endl;
+    }
+
+    std::cout << "The correct answer is " << ANSWER << "." << std::endl;
+    std::cout << "\nDone." << std::endl;
+}
